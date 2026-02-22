87a7a64e6c4ad0d856e6e403e739325e6b0b5592
Александр Чугуевский <a1998ch@gmail.com>
2026-02-22 14:00:40 +1000

    feat (api): added CriterionRule and CriterionBuilder classes

---
+++ ApfBuilder/Services/CriterionBuilder.cs
@@ -0,0 +1,21 @@
+﻿using ApfBuilder.Criteria;
+using ApfBuilder.Criteria.Core.Interfaces;
+using System.Collections.Generic;
+
+namespace ApfBuilder.Services
+{
+    public class CriterionBuilder
+    {
+        public Dictionary<CriterionCase, List<ICriterion>> ByCase { get; }
+
+        public List<ICriterion[]> ByComplexSelector { get; }
+
+        public CriterionBuilder(Dictionary<CriterionCase,
+            List<ICriterion>> byCase,
+            List<ICriterion[]> byComplexSelector)
+        {
+            ByCase = byCase;
+            ByComplexSelector = byComplexSelector;
+        }
+    }
+}
