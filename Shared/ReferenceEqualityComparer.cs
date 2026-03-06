187d54f49c87a8d7cd28aa34d50da0b5311d6394
Александр Чугуевский <a1998ch@gmail.com>
2026-03-06 09:58:47 +0300

    feat(api): Shared library added

---
+++ Shared/ReferenceEqualityComparer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Shared
+{
+    public sealed class ReferenceEqualityComparer<T> : IEqualityComparer<T>
+        where T : class
+    {
+        public static readonly ReferenceEqualityComparer<T> Instance =
+            new ReferenceEqualityComparer<T>();
+
+        public bool Equals(T x, T y) => ReferenceEquals(x, y);
+
+        public int GetHashCode(T obj) => RuntimeHelpers.GetHashCode(obj);
+    }
+}
